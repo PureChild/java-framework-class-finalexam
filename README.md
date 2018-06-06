50f1c91efad41261ddfa916a128697a12f0f5ea5
purechild <a01056490114@gmail.com>
2018-06-06 11:49:00 +0900

    reset

--- README.md
+++
@@ -1,5 +0,0 @@
-# Spring Framework 기말고사
-
-### @MVC
-
-2013108200 이승수
--- src/main/java/com/example/sympathy/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sympathy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-}
